babbd8c68f923ef41e1d99d8334ff8bbf078da73
JangJinHyuk <jhjang@jangboo.kr>
2020-07-30 19:02:09 +0900

    [HW] apply baseFragment && coroutine splash
    각 프래그먼트에 Base적용 및 코루틴을 이용하여 2초 후 홈 화면 진입하도록 적용

--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/home/HomeFragment.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/home/HomeFragment.kt
@@ -1,24 +1,12 @@
 package com.ironelder.metaweatherproject.ui.home
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.ironelder.metaweatherproject.R
+import com.ironelder.metaweatherproject.ui.base.BaseFragment
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+class HomeFragment : BaseFragment(R.layout.fragment_home) {
 
 }
--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/splash/SplashFragment.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/splash/SplashFragment.kt
@@ -2,23 +2,22 @@ package com.ironelder.metaweatherproject.ui.splash
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
+import androidx.navigation.fragment.findNavController
 import com.ironelder.metaweatherproject.R
+import com.ironelder.metaweatherproject.ui.base.BaseFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-/**
- * A simple [Fragment] subclass.
- */
-class SplashFragment : Fragment() {
+class SplashFragment : BaseFragment(R.layout.fragment_splash) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
     }
 
 }
