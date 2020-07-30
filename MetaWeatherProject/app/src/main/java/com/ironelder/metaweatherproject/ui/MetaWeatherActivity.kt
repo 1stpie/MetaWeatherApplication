8366b3cbbaea82f6ae74c8064d98300f1985bbd8
JangJinHyuk <jhjang@jangboo.kr>
2020-07-30 19:01:14 +0900

    [HW] apply navigation host in activity
    액티비티에 네이베이션 호스트 적용

--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/MetaWeatherActivity.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/MetaWeatherActivity.kt
@@ -1,9 +1,21 @@
 package com.ironelder.metaweatherproject.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.ironelder.metaweatherproject.R
 import com.ironelder.metaweatherproject.ui.base.BaseActivity
 
 class MetaWeatherActivity : BaseActivity(R.layout.activity_meta_weather) {
+
+    private val navController: NavController by lazy {
+        findNavController(R.id.nav_host_fragment)
+    }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
 }
