7933b50d9fca3fbb2b3d55fb10bc3d64dd675cec
JangJinHyuk <jhjang@jangboo.kr>
2020-07-30 15:03:45 +0900

    [HW] koin setting
    Koin DI를 사용하기 위한 설정 추가

--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/application/MetaWeatherApplication.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/application/MetaWeatherApplication.kt
@@ -1,11 +1,22 @@
 package com.ironelder.metaweatherproject.ui.application
 
 import android.app.Application
+import com.ironelder.metaweatherproject.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MetaWeatherApplication : Application(){
     override fun onCreate() {
         super.onCreate()
-        //TODO : start Koin
+
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.DEBUG else Level.NONE)
+            androidContext(this@MetaWeatherApplication)
+            modules(
+            )
+        }
     }
 
 }
