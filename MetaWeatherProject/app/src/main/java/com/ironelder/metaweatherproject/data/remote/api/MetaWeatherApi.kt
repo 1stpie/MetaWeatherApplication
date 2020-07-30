5b65e1d76caf5b162d54350848a3034270844c87
JangJinHyuk <jhjang@jangboo.kr>
2020-07-30 18:34:28 +0900

    [HW] Remote Model Apply
    리모트 모델이 사용될 레이어 적용

--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/data/remote/api/MetaWeatherApi.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/data/remote/api/MetaWeatherApi.kt
@@ -1,15 +1,15 @@
 package com.ironelder.metaweatherproject.data.remote.api
 
-import com.ironelder.metaweatherproject.data.model.LocationData
-import com.ironelder.metaweatherproject.data.model.WeatherData
+import com.ironelder.metaweatherproject.data.remote.model.LocationModel
+import com.ironelder.metaweatherproject.data.remote.model.WeatherModel
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MetaWeatherApi {
     @GET("api/location/search/")
-    suspend fun getLocation(@Query("query") query: String): List<LocationData>
+    suspend fun getLocation(@Query("query") query: String): List<LocationModel>
 
     @GET("api/location/{locationId}/")
-    suspend fun getWeather(@Path("locationId") locationId: String): WeatherData
+    suspend fun getWeather(@Path("locationId") locationId: String): WeatherModel
 }
--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/domain/repository/MetaWeatherRepository.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/domain/repository/MetaWeatherRepository.kt
@@ -1,4 +1,9 @@
 package com.ironelder.metaweatherproject.domain.repository
 
+import com.ironelder.metaweatherproject.domain.model.LocationModel
+import com.ironelder.metaweatherproject.domain.model.WeatherModel
+
 interface MetaWeatherRepository : MetaRepository {
+    suspend fun getLocation(query: String): List<LocationModel>
+    suspend fun getWeather(locationId: String): WeatherModel
 }
