ffa627fe16e438c3142bc56df136af213aad05dc
JangJinHyuk <jhjang@jangboo.kr>
2020-08-03 16:43:07 +0900

    [HW] Home UI Model Add
    홈 화면에서 사용할 모델 추가

---
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/ui/home/model/HomeWeatherModel.kt
@@ -0,0 +1,16 @@
+package com.ironelder.metaweatherproject.ui.home.model
+
+data class HomeWeatherModel(
+
+    val title: String,
+    val detailId: Int,
+    val todayHumidity: String,
+    val todayTemp: String,
+    val todayWeatherName: String,
+    val todayImageUrl: String,
+    val tomorrowHumidity: String,
+    val tomorrowTemp: String,
+    val tomorrowWeatherName: String,
+    val tomorrowImageUrl: String
+
+)
