60563527ccde5e0297e0f6e51c27acfde4218813
JangJinHyuk <jhjang@jangboo.kr>
2020-08-03 19:43:13 +0900

    [HW]DataBinding Ext Add
    데이터 바인딩 확장함수 및 화면 스트링 적용

--- MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/util/ext/View.kt
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/util/ext/View.kt
@@ -1,2 +1,18 @@
 package com.ironelder.metaweatherproject.util.ext
 
+import android.view.View
+import androidx.databinding.BindingAdapter
+import io.reactivex.subjects.Subject
+
+@BindingAdapter("visibleOrInvisible")
+fun View.bindVisibleOrInvisible(isVisible: Boolean) {
+    println("visibleOrInvisible = $isVisible")
+    this.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("onClick")
+fun View.bindClick(onClick: Subject<Unit>) {
+    this.setOnClickListener {
+        onClick.onNext(Unit)
+    }
+}
