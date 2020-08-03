dcf4d2fc0c93b6311efd73c386ae6f45ab0e0eb2
JangJinHyuk <jhjang@jangboo.kr>
2020-08-03 10:29:37 +0900

    [HW] Repository Implement
    api 연동을 위한 repository에 대한 매퍼를 구현하여 구체화

---
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/data/remote/MetaWeatherRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.ironelder.metaweatherproject.data.remote
+
+import com.ironelder.metaweatherproject.data.mapper.LocationDataMapper
+import com.ironelder.metaweatherproject.data.mapper.WeatherDataMapper
+import com.ironelder.metaweatherproject.data.remote.source.MetaWeatherDataSource
+import com.ironelder.metaweatherproject.domain.model.LocationModel
+import com.ironelder.metaweatherproject.domain.model.WeatherModel
+import com.ironelder.metaweatherproject.domain.repository.MetaWeatherRepository
+
+class MetaWeatherRepositoryImpl(private val weatherRemoteDataSource: MetaWeatherDataSource) :
+    MetaWeatherRepository {
+    override suspend fun getLocation(query: String): List<LocationModel> {
+        return weatherRemoteDataSource.getLocation(query).map(LocationDataMapper::mapToModel)
+    }
+
+    override suspend fun getWeather(locationId: String): WeatherModel {
+        return WeatherDataMapper.mapToModel(weatherRemoteDataSource.getWeather(locationId))
+    }
+}
