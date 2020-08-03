7ef4f39863243a7f1f95cbcea9901dcddb686f25
JangJinHyuk <jhjang@jangboo.kr>
2020-08-03 17:19:14 +0900

    [HW] NonNullble LivaData Add
    UI 표현을 위한 널을 허용하지 않는 라이브데이터 유틸 추가

---
+++ MetaWeatherProject/app/src/main/java/com/ironelder/metaweatherproject/util/NotNullMutableLiveData.kt
@@ -0,0 +1,11 @@
+package com.ironelder.metaweatherproject.util
+
+import androidx.lifecycle.MutableLiveData
+
+class NotNullMutableLiveData<E : Any>(
+    defaultValue: E
+) : MutableLiveData<E>(defaultValue) {
+    override fun getValue(): E {
+        return super.getValue()!!
+    }
+}
